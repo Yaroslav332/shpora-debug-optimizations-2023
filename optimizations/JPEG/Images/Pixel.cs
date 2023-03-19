8f8eb190e093dcc5fe7103183347c6fed5b49ca1
FoxDictator <mega.y1979@gmail.com>
2023-03-19 21:27:22 +0500

    changed Pixel class

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -1,46 +1,50 @@
-﻿using System;
-using System.Linq;
-
-namespace JPEG.Images;
+﻿namespace JPEG.Images;
 
 public class Pixel
 {
-	private readonly PixelFormat format;
+    private readonly PixelFormat format;
 
-	public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
-	{
-		format = pixelFormat;
-		if (pixelFormat == PixelFormat.RGB)
-		{
-			r = firstComponent;
-			g = secondComponent;
-			b = thirdComponent;
-		}
+    public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
+    {
+        format = pixelFormat;
+        if (pixelFormat == PixelFormat.RGB)
+        {
+            r = firstComponent;
+            g = secondComponent;
+            b = thirdComponent;
 
-		if (pixelFormat == PixelFormat.YCbCr)
-		{
-			y = firstComponent;
-			cb = secondComponent;
-			cr = thirdComponent;
-		}
-	}
+            y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+            cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+            cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        }
 
-	private readonly double r;
-	private readonly double g;
-	private readonly double b;
+        if (pixelFormat == PixelFormat.YCbCr)
+        {
+            y = firstComponent;
+            cb = secondComponent;
+            cr = thirdComponent;
 
-	private readonly double y;
-	private readonly double cb;
-	private readonly double cr;
+            r = (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
+            g = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+            b = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+        }
+    }
 
-	public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
+    private readonly double r;
+    private readonly double g;
+    private readonly double b;
 
-	public double G =>
-		format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+    private readonly double y;
+    private readonly double cb;
+    private readonly double cr;
 
-	public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+    public double R => r;
 
-	public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-	public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-	public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+    public double G => g;
+
+    public double B => b;
+
+    public double Y => y;
+    public double Cb => cb;
+    public double Cr => cr;
 }
