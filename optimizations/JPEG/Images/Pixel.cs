e9ffce40e570d8f8fec3c4905caff2eba5fd7b86
FoxDictator <mega.y1979@gmail.com>
2023-03-19 23:01:59 +0500

    changed Pixel back

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -3,6 +3,7 @@
 public class Pixel
 {
     private readonly PixelFormat format;
+    private bool isRGB;
 
     public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
     {
@@ -12,21 +13,14 @@ public class Pixel
             r = firstComponent;
             g = secondComponent;
             b = thirdComponent;
-
-            y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-            cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-            cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+            isRGB = true;
         }
-
-        if (pixelFormat == PixelFormat.YCbCr)
+        else if (pixelFormat == PixelFormat.YCbCr)
         {
             y = firstComponent;
             cb = secondComponent;
             cr = thirdComponent;
-
-            r = (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-            g = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-            b = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+            isRGB = false;
         }
     }
 
@@ -38,13 +32,13 @@ public class Pixel
     private readonly double cb;
     private readonly double cr;
 
-    public double R => r;
+    public double R => isRGB ? r : (298.082 * Y + 408.583 * Cr) / 256.0 - 222.921;
 
-    public double G => g;
+    public double G => isRGB? g: (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
 
-    public double B => b;
+    public double B => isRGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
 
-    public double Y => y;
-    public double Cb => cb;
-    public double Cr => cr;
+    public double Y => isRGB ? 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0 : y;
+    public double Cb => isRGB ? 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0 : cb;
+    public double Cr => isRGB ? 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0 : cr;
 }
